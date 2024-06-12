d24545eb3b7b81675a075d54312d864bc028a6ea
lyt111111 <321322324@qq.com>
2024-06-12 21:10:59 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+axios.defaults.baseURL = 'http://localhost:8081'
+// 将 axios 实例挂载到 Vue 的原型上
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
+
+Vue.use(ElementUI);
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
